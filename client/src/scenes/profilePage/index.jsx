ad39d5abb8ce9c5bfab2cc86fa8b3caf6cd00225
lioritah <lioritah@gmail.com>
2023-09-18 01:49:28 +0300

    first commit

--- client/src/scenes/profilePage/index.jsx
+++ client/src/scenes/profilePage/index.jsx
@@ -43,11 +43,7 @@ const ProfilePage = () => {
         <Box flexBasis={isNonMobileScreens ? "26%" : undefined}>
           <UserWidget userId={userId} picturePath={user.picturePath} />
           <Box m="2rem 0" />
-          {userId === loggedInUserId ? (
-            <FriendListWidget userId={userId} />
-          ) : (
-            <></>
-          )}
+          <FriendListWidget userId={userId} />
         </Box>
         <Box
           flexBasis={isNonMobileScreens ? "42%" : undefined}
