8807b3b991a5bc03a1bef71c0ef47d962ca0a4e1
lioritah <lioritah@gmail.com>
2023-09-18 01:48:16 +0300

    first commit

--- client/src/state/index.js
+++ client/src/state/index.js
@@ -1,15 +1,40 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { getFriendsServer } from "services/api.service";
 
 const initialState = {
   mode: "light",
   user: null,
+  friendsByUserId: {},
   token: null,
   posts: [],
 };
 
+const getFriendsByUserId = createAsyncThunk(
+  "users/getFriendsByUserId",
+  async (userId, thunkAPI) => {
+    const state = thunkAPI.getState();
+    if (state.friendsByUserId[userId]) return undefined; // already cached the user's friends
+    const response = await getFriendsServer(userId, state.token);
+    return {
+      friends: response,
+      userId,
+    };
+  }
+);
 export const authSlice = createSlice({
   name: "auth",
   initialState,
+  extraReducers: (builder) => {
+    builder.addCase(getFriendsByUserId.fulfilled, (state, action) => {
+      if (!state.action.payload) return; // cache was already done for this user's friends
+      state.friendsByUserId = {
+        ...state.friendsByUserId,
+        [action.payload.userId]: action.payload.friends,
+      };
+    });
+    builder.addCase(getFriendsByUserId.rejected, (state, action) => {});
+    builder.addCase(getFriendsByUserId.pending, (state, action) => {});
+  },
   reducers: {
     setMode: (state) => {
       state.mode = state.mode === "light" ? "dark" : "light";
@@ -24,11 +49,12 @@ export const authSlice = createSlice({
       state.token = null;
     },
     setFriends: (state, action) => {
-      if (state.user) {
-        state.user.friends = action.payload.friends;
-      } else {
-        console.error("user friends non-existent :(");
-      }
+      state.friendsByUserId[action.payload.userId] = action.payload.friends;
+    },
+    patchFriendsBi: (state, action) => {
+      state.friendsByUserId[action.payload.friendId] =
+        action.payload.friendsOtherUser;
+      state.friendsByUserId[action.payload.userId] = action.payload.userFriends;
     },
     setPosts: (state, action) => {
       state.posts = action.payload.posts;
@@ -43,6 +69,14 @@ export const authSlice = createSlice({
   },
 });
 
-export const { setMode, setLogin, setLogout, setFriends, setPosts, setPost } =
-  authSlice.actions;
+export const {
+  setMode,
+  setLogin,
+  setLogout,
+  setFriends,
+  setPosts,
+  setPost,
+  patchFriendsBi,
+} = authSlice.actions;
+export { getFriendsByUserId };
 export default authSlice.reducer;
